7569709019b16375eb7d7dee541b084416e9ea2c
Chase Terry <chase.terry87@gmail.com>
2021-09-13 17:29:12 -0500

    Implemented tailwind landing page with fontawesome

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,23 @@ module.exports = {
             fontFamily: {
                 sans: ['Nunito', ...defaultTheme.fontFamily.sans],
             },
+            colors: {
+                'bookmark-purple': '#00a1e4',
+                'bookmark-red': '#FF0066',
+                'bookmark-blue': '#122640',
+                'bookmark-grey': '#373F51',
+                'bookmark-white': '#efefef',
+            }
         },
+        container: {
+            center: true,
+            padding: '1rem',
+            screens: {
+                lg: '1124px',
+                xl: '1124px',
+                '2xl': '1124px'
+            }
+        }
     },
 
     variants: {
