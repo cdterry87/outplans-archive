bec75120ae36914b36c8343595cd69b1d1b3be5d
Chase Terry <chase.terry87@gmail.com>
2021-09-13 22:51:10 -0500

    Updated welcome page with Tailwind and added fontawesome icons

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,22 @@
+<footer class="px-4 pt-12 pb-8 bg-white border-t border-gray-200">
+    <div class="container mt-6 flex flex-col justify-between max-w-6xl mx-auto overflow-hidden lg:flex-row">
+        <p class="text-base text-gray-500 text-center lg:text-left">
+            <a href="/" class="flex font-bold mb-6 text-xl justify-start block text-left sm:text-center lg:text-left sm:justify-center lg:justify-start">
+                Eleplans
+            </a>
+            Simplifying the way you make plans!
+        </p>
+        <div class="flex block w-full items-end text-center justify-between mt-10 text-sm lg:w-3/5 sm:flex lg:mt-0 gap-6">
+            <a href="/about" class="flex w-full font-medium text-gray-700">About Us</a>
+            <a href="/privacy" class="flex w-full font-medium text-gray-700">Privacy Policy</a>
+            <a href="/terms" class="flex w-full font-medium text-gray-700">Our Terms</a>
+            <a href="/contact" class="flex w-full font-medium text-gray-700">Contact Us</a>
+        </div>
+    </div>
+    <div class="mt-12 flex justify-center text-3xl text-gray-500 gap-6">
+        <a href="https://facebook.com" class="hover:text-gray-700"><i class="fab fa-facebook"></i></a>
+        <a href="https://twitter.com" class="hover:text-gray-700"><i class="fab fa-twitter"></i></a>
+        <a href="https://instagram.com" class="hover:text-gray-700"><i class="fab fa-instagram"></i></a>
+    </div>
+    <div class="pt-4 pt-6 mt-10 text-center text-gray-400 border-t border-gray-100">&copy; Eleplans 2021</div>
+</footer>
---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,23 @@
+<header class="relative z-50 w-full h-24">
+    <div class="container flex items-center h-full max-w-6xl px-8 mx-auto sm:justify-between xl:px-0">
+        <a href="/" class="relative flex items-center inline-block h-5 h-full font-black leading-none">
+            <span class="ml-3 text-4xl text-gray-800">Eleplans</span>
+        </a>
+        <div
+            class="absolute left-0 flex-col items-center justify-center hidden w-full pb-8 mt-48 border-b border-gray-200 md:relative md:w-auto md:bg-transparent md:border-none md:mt-0 md:flex-row md:p-0 md:items-end md:flex md:justify-between">
+            <a href="/login"
+               class="relative z-40 inline-block w-auto h-full px-5 py-3 text-lg font-bold leading-none transition-all transition duration-100 duration-300 rounded fold-bold sm:w-full lg:shadow-none">
+                Login
+            </a>
+            <a href="/register"
+               class="relative z-40 inline-block w-auto h-full px-5 py-3 text-lg font-bold leading-none transition-all transition duration-100 duration-300 rounded fold-bold sm:w-full lg:shadow-none">
+                Register
+            </a>
+        </div>
+        <div id="nav-mobile-btn"
+             class="absolute top-0 right-0 z-50 block w-6 mt-8 mr-10 cursor-pointer select-none md:hidden sm:mt-10">
+            <span class="block w-full h-1 mt-2 duration-200 transform bg-gray-800 rounded-full sm:mt-1"></span>
+            <span class="block w-full h-1 mt-1 duration-200 transform bg-gray-800 rounded-full"></span>
+        </div>
+    </div>
+</header>
