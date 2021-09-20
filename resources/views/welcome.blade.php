4f3881ac543f0c2b0375e074fedd1c72858c7613
Chase Terry <chase.terry87@gmail.com>
2021-09-19 20:53:36 -0500

    Modified button style and fixed welcome page margins

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,12 @@
 <x-guest-layout>
     <x-header />
 
-    <div class="relative items-center justify-center w-full overflow-x-hidden py-12 lg:py-32 px-8">
+    <div class="relative items-center justify-center w-full max-w-6xl px-8 py-12 mx-auto overflow-x-hidden lg:py-32">
         <div class="container flex flex-col items-center">
-            <h1 class="relative mb-4 text-3xl font-black text-center leading-tight text-gray-900 sm:text-6xl xl:mb-8">
+            <h1 class="relative mb-4 text-3xl font-black leading-tight text-center text-gray-900 sm:text-6xl xl:mb-8">
                 Start Making Plans!
             </h1>
-            <p class="pr-0 mb-8 text-base text-gray-600 sm:text-lg xl:text-xl lg:pr-20 text-center">
+            <p class="pr-0 mb-8 text-base text-center text-gray-600 sm:text-lg xl:text-xl lg:pr-20">
                 Making and sharing plans with your friends has never been simpler!
             </p>
             <a href="/register" class="button button-black">
