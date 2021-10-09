b2ffa7a6872da0e356a6c49b93375ad26b4cc1f5
Chase Terry <chase.terry87@gmail.com>
2021-10-09 01:48:00 -0500

    Moved main database seeding logic from UserSeeder to DatabaseSeeder so UserSeeder can focus on seeding only Users and DatabaseSeeder can be the master seeder for the full database.

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,12 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\Friend;
-use App\Models\FriendInvite;
-use App\Models\Message;
-use App\Models\Plan;
-use App\Models\PlanAttendee;
-use App\Models\PlanInvite;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -20,40 +14,6 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        // Create a guest account
-        $guest = User::factory()->create([
-            'name' => 'Guest',
-            'email' => 'guest@example.com',
-        ]);
-
-        // For the guest account, create some plans
-        $guest->plans()->saveMany(Plan::factory()->count(5)->create([
-            'user_id' => $guest->id
-        ])->each(function ($plan) use (&$guest) {
-            // For each plan add some attendees...
-            $plan->attendees()->saveMany(PlanAttendee::factory()->count(5)->create([
-                'plan_id' => $plan->id
-            ]));
-            // ...and send some plan invites
-            $plan->invites()->saveMany(PlanInvite::factory()->count(5)->create([
-                'user_id' => $guest->id,
-                'plan_id' => $plan->id
-            ]));
-            // ...and finally display some messages for each plan
-            $guest->messages()->saveMany(Message::factory()->count(2)->create([
-                'user_id' => $guest->id,
-                'plan_id' => $plan->id
-            ]));
-        }));
-
-        // The guest should also have some friends
-        $guest->friends()->saveMany(Friend::factory()->count(5)->create([
-            'user_id' => $guest->id,
-        ]));
-
-        // And the guest should also have sent some friend invites
-        $guest->friends_invites()->saveMany(FriendInvite::factory()->count(5)->create([
-            'user_id' => $guest->id,
-        ]));
+        User::factory(10)->create();
     }
 }
